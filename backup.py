aeb1d8cf02a5428247efac3213ff159f96797465
AKU <tom@tdpain.net>
2022-02-11 19:54:37 +0000

    Add option to provide a config file via the command line
    Signed-off-by: AKU <tom@tdpain.net>

--- backup.py
+++ backup.py
@@ -2,15 +2,18 @@ import subprocess
 import os
 import datetime
 import json
+import sys
+
+CONFIG_FILE = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.path.expandvars("$HOME"), "backupConfig.json")
 
 datestring = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d_%Hh%Mm")
 
-filename = f"server_{datestring}.tar.gz"
-to_backup = None
-with open(os.path.join(os.path.expandvars("$HOME"), "backupConfig.json")) as f:
-    to_backup = json.load(f)
+with open(CONFIG_FILE) as f:
+    config = json.load(f)
 
-subprocess.run(['tar', '-czvf', filename,] + to_backup)
+filename = config.get("filenameTemplate", "backup_{}.tar.gz").format(datestring)
+
+subprocess.run(['tar', '-czvf', filename] + config.get("files", []))
 subprocess.run(['rclone', 'copy', "-v", filename, 'gd:/server'])
 os.remove(filename)
 subprocess.run(['rclone', 'delete', "-v", '--min-age', '15d', 'gd:/server'])
